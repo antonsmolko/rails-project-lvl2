3cb56a93c1af4fd0a6d3a0b43a307560f5acc9ea
Anton Smolko <asmolko@yandex.ru>
2021-11-10 00:47:58 +0300

    step 7 completed: update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   root 'posts#index'
-  resources :posts, param: :id do
+  resources :posts do
     scope module: :posts do
       resources :comments, only: %w[create new], shallow: true
       resources :likes, only: %w[create destroy], shallow: true
